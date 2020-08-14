00c483abb3f23de1468e5d0b02a4fd9dbb304471
Christen Ford <christen0ford@gmail.com>
2020-08-13 20:12:02 -0400

    initial working draft of contract

--- contracts/ipfs_ks/lib.rs
+++ contracts/ipfs_ks/lib.rs
@@ -2,197 +2,240 @@
 
 use ink_lang as ink;
 
-
 #[ink::contract(version = "0.1.0")]
 mod ipfs_ks {
 
-    use ipfs_coin::IpfsCoin;
-
     #[cfg(not(feature = "ink-as-dependency"))]
     use ink_core::storage2::{
-        collections::HashMap as StorageHashMap,
-        collections::Vec as StorageVector,
-        lazy::Lazy
+        collections::{
+            HashMap as InkHashMap
+        }
     };
 
+    pub enum TxOp {
+        Create,
+        Read,
+        Write
+    }
+
     #[ink(event)]
-    struct RequestUploadEvent {
-        account: AccountId,
+    struct FileCreated {
+        #[ink(topic)]
+        owner: AccountId,
+        #[ink(topic)]
         hash: Hash,
+        #[ink(topic)]
+        ts: Timestamp
     }
 
     #[ink(event)]
-    struct RequestRemoveEvent {
-        account: AccountId,
+    struct FileRemoved {
+        #[ink(topic)]
         hash: Hash,
+        #[ink(topic)]
+        ts: Timestamp
     }
 
     #[ink(event)]
-    struct RequestModifyEvent {
-        account: AccountId,
-        hash: Hash
-    }
-
-    #[ink(event)]
-    struct RequestReadEvent {
-        account: AccountId,
+    struct FileModified {
+        #[ink(topic)]
         hash: Hash,
+        #[ink(topic)]
+        ts: Timestamp
     }
 
-    enum IpfsPermission {
-        READ,
-        WRITE
+    #[ink(event)]
+    struct FileRead {
+        #[ink(topic)]
+        hash: Hash,
+        #[ink(topic)]
+        ts: Timestamp
     }
 
-    struct IpfsFile {
-        author: AccountId,
-        perms: StorageHashMap<AccountId, (bool, bool)>,
-        origin_ts: Timestamp,
-    }
-
-    impl IpfsFile {
-        fn new(author: &AccountId) -> Self {
-            let now = Self::env.block_timestamp();
-            Self {
-                author: *author,
-                perms: StorageHashMap::new(),
-                origin_ts: now,
-            }
-        }
-
-        fn is_author(&self, account: &AccountId) -> bool {
-            *account == *self.author
-        }
-
-        fn has_permission(
-            &self, 
-            account: &AccountId, 
-            perm: IpfsPermission) 
-        -> bool {
-            let mut result = false;
-            if *self.perms.contains_key(*account) {
-                result = match perm {
-                    READ => *self.perms.get(*account).0,
-                    WRITE => *self.perms.get(*account).1,
-                    _ => false
-                }
-            }
-            result
-        }
-
-        fn add_permission(
-            &self,
-            account: &AccountId,
-            perm: IpfsPermission) 
-        -> bool {
-            let mut result = false;
-            if *self.perms.contains_key(*account) {
-                let acct_perms = *self.perms.take(*account);
-                let new_acct_perms = match perm {
-                    READ => (true, acct_perms[1]]),
-                    WRITE => (acct_perms[0], true),
-                    _ => return false
-                }
-                *self.perms.insert(*account, new_acct_perms);
-                result = true;
-            } else {
-                let new_acct_perms = match perm {
-                    READ => (true, false),
-                    WRITE => (false, true),
-                    _ => return false
-                }
-                *self.perms.insert(*account, new_acct_perms);
-                result = true;
-            }
-            result
-        }
-
-        fn remove_permission(
-            &self,
-            account: &AccountId,
-            perm: IpfsPermission) 
-        -> bool {
-            let mut result = false;
-            if *self.perms.contains_key(*account) {
-                let acct_perms = *self.perms.take(*account);
-                let new_acct_perms = match perm {
-                    READ => (false, acct_perms[1]]),
-                    WRITE => (acct_perms[0], false),
-                    _ => return false
-                }
-                *self.perms.insert(*account, new_acct_perms);
-                result = true;
-            }
-            result
-        }
-    }
-
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
     #[ink(storage)]
     struct IpfsKs {
-        ipfs_coin: Lazy<IpfsCoin>,
-        files: StorageHashMap<Hash, IpfsFile>,
+        balances: InkHashMap<AccountId, Balance>,
+        // first u32 is location, 2nd is number of ops performed on it
+        files: InkHashMap<Hash, (AccountId, i32)>
     }
 
     impl IpfsKs {
+
         #[ink(constructor)]
-        fn new(
-            ipfs_coin_code_hash: Hash, 
-            initial_supply: Balance) 
-        -> Self {
-            let total_balance = Self::env().balance();
-            let ipfs_coin = IpfsCoin::new(initial_supply, &Self::env().caller())
-                .endowment(total_balance/2)
-                .using_code(ipfs_coin_code_hash)
-                .instantiate()
-                .expect("failed at instantiating the `IpfsCoin` contract");
+        fn new() -> Self {
             Self {
-                ipfs_coin: Lazy::new(ipfs_coin),
-                files: StorageHashMap::new()
+                balances: InkHashMap::new(),
+                files: InkHashMap::new()
             }
+        }
+        
+        #[ink(message)]
+        fn deposit(&mut self, value: Balance) -> bool {
+            let caller = self.env().caller();
+
+            if !self.balances.get(&caller).is_some() {
+                return false
+            }
+
+            let balance = self.balance_or_zero(&caller);
+            self.balances.insert(caller, balance + value);
+
+            true
+        }
+        
+        #[ink(message)]
+        fn withdraw(&mut self, value: Balance) -> Balance {
+            let caller = self.env().caller();
+
+            if !self.balances.get(&caller).is_some() {
+                return 0
+            }
+
+            let balance = self.balance_or_zero(&caller);
+            if value > balance {
+                return 0
+            }
+            self.balances.insert(caller, balance - value);
+
+            value
+        }
+        
+        #[ink(message)]
+        fn balance(&self) -> Balance {
+            let caller = self.env().caller();
+
+            if !self.balances.get(&caller).is_some() {
+                return 0
+            }
+
+            self.balance_or_zero(&caller)
+        }
+
+        fn charge(&mut self, account: &AccountId, hash: &Hash, op: TxOp) -> bool {
+            let base_cost: f32 = match op {
+                TxOp::Create => 3., 
+                TxOp::Read => 2.,
+                TxOp::Write => 3.
+            };
+            let t = self.files.get(&hash).unwrap().1 as f32;
+            let cost = (base_cost + 1.07 * t) as u128;
+            let balance = self.balance_or_zero(account);
+            if cost > balance {
+                return false
+            }
+            self.balances.insert(*account, balance - cost);
+            // the difference here should be added to the contract accounts balance
+            true
         }
 
         #[ink(message)]
-        fn request_upload(&mut self, hash: &Hash) -> bool {
-            let caller = Self::env().caller();
-            Self::env.emit_event(RequestUploadEvent {
-                caller,
-                hash,
+        fn add_file(&mut self, hash: Hash) -> bool {
+            let caller = self.env().caller();
+
+            if !self.balances.get(&caller).is_some() {
+                return false
+            }
+
+            if self.files.get(&hash).is_some() {
+                return false
+            }
+
+            if !self.charge(&caller, &hash, TxOp::Create) {
+                return false
+            }
+
+            let timestamp = self.env().block_timestamp();
+            self.files.insert(hash, (caller, 0));
+            self.env().emit_event(FileCreated {
+                owner: caller,
+                hash: hash,
+                ts: timestamp
             });
+
             true
         }
-        
+
         #[ink(message)]
-        fn request_remove(&mut self, hash: &Hash) {
-            let caller = Self::env().caller();
-            Self::env.emit_event(RequestRemoveEvent {
-                caller,
-                hash,
+        fn remove_file(&mut self, hash: Hash) -> bool  {
+            let caller = self.env().caller();
+
+            if !self.balances.get(&caller).is_some() {
+                return false
+            }
+
+            if !self.files.get(&hash).is_some() {
+                return false
+            }
+
+            let timestamp = self.env().block_timestamp();
+            self.env().emit_event(FileRemoved {
+                hash: hash,
+                ts: timestamp
             });
+
             true
         }
-        
+
         #[ink(message)]
-        fn request_modify(&mut self, hash: &Hash) {
-            let caller = Self::env().caller();
-            Self::env.emit_event(RequestModifyEvent {
-                caller,
-                hash,
+        fn write_file(&mut self, hash: Hash) -> bool {
+            let caller = self.env().caller();
+
+            if !self.balances.get(&caller).is_some() {
+                return false
+            }
+
+            if !self.files.get(&hash).is_some() {
+                return false
+            }
+
+            if !self.charge(&caller, &hash, TxOp::Write) {
+                return false
+            }
+
+            let stats = *self.files.get(&hash).unwrap();
+            self.files.insert(hash, (stats.0, stats.1 + 1));
+
+            let timestamp = self.env().block_timestamp();
+            self.env().emit_event(FileModified {
+                hash: hash,
+                ts: timestamp
             });
+
             true
         }
-        
+
         #[ink(message)]
-        fn request_read(&self, hash: &Hash) {
-            let caller = Self::env().caller();
-            Self::env.emit_event(RequestReadEvent {
-                caller,
-                hash,
+        fn read_file(&mut self, hash: Hash) -> bool {
+            let caller = self.env().caller();
+
+            if !self.balances.get(&caller).is_some() {
+                return false
+            }
+
+            if !self.files.get(&hash).is_some() {
+                return false
+            }
+
+            if !self.charge(&caller, &hash, TxOp::Read) {
+                return false
+            }
+
+            let stats = *self.files.get(&hash).unwrap();
+            self.files.insert(hash, (stats.0, stats.1 + 1));
+
+            let timestamp = self.env().block_timestamp();
+            self.env().emit_event(FileRead {
+                hash: hash,
+                ts: timestamp
             });
+
             true
         }
+
+        fn balance_or_zero(&self, owner: &AccountId) -> Balance {
+            *self.balances.get(&owner).unwrap_or(&0)
+        }
+
     }
 
     /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
